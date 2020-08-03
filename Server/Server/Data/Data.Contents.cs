43f5bf58ed8beeefbce666fdc994e8726863941e
Rookiss <rookiss@naver.com>
2020-08-03 17:05:34 +0900

    Day17) Data & Config

--- Server/Server/Data/Data.Contents.cs
+++ Server/Server/Data/Data.Contents.cs
@@ -1,12 +1,11 @@
 ﻿using Google.Protobuf.Protocol;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Text;
 
-namespace Data
-{ 
-#region Stat
+namespace Server.Data
+{
+	#region Stat
 	[Serializable]
 	public class Stat
 	{
