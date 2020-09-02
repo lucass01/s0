4a5d6a93362978db240c55814a500dfbd6ad3d33
Rookiss <rookiss@naver.com>
2020-09-02 17:57:17 +0900

    Day08) 아이템 착용 #1 #2

--- Server/Server/DB/DbTransaction.cs
+++ Server/Server/DB/DbTransaction.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace Server.DB
 {
-	public class DbTransaction : JobSerializer
+	public partial class DbTransaction : JobSerializer
 	{
 		public static DbTransaction Instance { get; } = new DbTransaction();
 
@@ -80,6 +80,9 @@ namespace Server.DB
 				return;
 
 			// TODO : 살짝 문제가 있긴 하다...
+			// 1) DB에다가 저장 요청
+			// 2) DB 저장 OK
+			// 3) 메모리에 적용
 			int? slot = player.Inven.GetEmptySlot();
 			if (slot == null)
 				return;
