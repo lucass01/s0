a4dd44c7ecaca0575208434393b128584afb547a
Rookiss <rookiss@naver.com>
2020-09-09 20:40:16 +0900

    Day13) A* 개선

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -78,20 +78,23 @@ namespace Server
 			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
-			// GameLogicTask
+			// DbTask
 			{
-				Task gameLogicTask = new Task(GameLogicTask, TaskCreationOptions.LongRunning);
-				gameLogicTask.Start();
+				Thread t = new Thread(DbTask);
+				t.Name = "DB";
+				t.Start();
 			}
 
 			// NetworkTask
 			{
-				Task networkTask = new Task(NetworkTask, TaskCreationOptions.LongRunning);
-				networkTask.Start();
+				Thread t = new Thread(NetworkTask);
+				t.Name = "Network Send";
+				t.Start();
 			}
 
-			// DbTask
-			DbTask();
+			// GameLogic
+			Thread.CurrentThread.Name = "GameLogic";
+			GameLogicTask();
 		}
 	}
 }
